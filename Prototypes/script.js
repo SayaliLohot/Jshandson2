0aac663ad4a4fea92934e284ddf7e09786872f5e
sayali <sayalinlohot@gmail.com>
2023-02-01 21:19:41 +0530

    add questions

--- Prototypes/script.js
+++ Prototypes/script.js
@@ -18,3 +18,27 @@ secondChild.location="Pune"
 secondChild.age="22"
 
 secondChild.details()
+
+//Q.2 Write code to explain prototype chaining
+const grandFather={
+    name:"Sudhir",
+    age:80
+}
+const father=Object.create(grandFather)
+console.log(father.name)//it will gives result as sudhir as name property is not to fater hence it will look for it in parent chain
+const son = Object.create(grandFather)
+son.name="Yogesh"
+console.log(son.name)//as we declase value of son.name hence it shows resullt as yogesh
+
+//Q.3 Add a method to calculate sum of all elements in Array in array's protype, use that method to calculate sum for multiple arrays
+let arr = new Array(1,2,3,4,5)
+arr.__proto__.sum=function(givenArray){
+    let count = 0;
+    for(let i =0;i<arr.length;i++){
+        count +=arr[i];
+    }
+    return count;
+}
+let result =arr.sum(arr)
+console.log("The sum of given array's element is ", result)
+//output-15
